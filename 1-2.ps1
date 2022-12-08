ee40f5dc7031aca53e1488b56584699e094f68a7
FeistyBilla <southmayd.will@gmail.com>
2022-12-07 21:32:30 -0800

    Update 1-2.ps1

--- 1-2.ps1
+++ 1-2.ps1
@@ -1,17 +1,16 @@
-$calories = Get-Content -Path ".\1-1.txt"
-$sumCalories = 0
+$sum_calories = 0
 $output = @()
-foreach ($calorie in $calories) {
-    if ($calorie) {
-        $sumCalories += $calorie
+Get-Content -Path ".\1-1.txt" | ForEach-Object -Process {
+    if ($_) {
+        $sum_calories += $_
     } else {
-        $sum = [PSCustomObject]@{
-            Elf_Total = $sumCalories
+        $elf_calories = [PSCustomObject]@{
+            elf_total = $sum_calories
         }
-        $output += $sum 
-        $sumCalories = 0
-    }
+    $output += $elf_calories 
+    $sum_calories = 0
+    }        
 }
-$output = $output | Sort-Object Elf_Total -Descending
-$top3_sum = ($output).Elf_Total[0] + ($output).Elf_Total[1] + ($output).Elf_Total[2]
-Write-Host $top3_sum -ForegroundColor DarkGreen
+$top_3 = 0
+($output | Sort-Object Elf_Total -Descending | Select-Object Elf_Total -First 3).Elf_Total | ForEach-Object {$top_3 += $_}
+Write-Host $top_3 -ForegroundColor DarkGreen
