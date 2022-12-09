d28bc97417b25ff36fa76d846f16e25abfe61737
FeistyBilla <southmayd.will@gmail.com>
2022-12-08 23:07:20 -0800

    Update 3-1.ps1

--- 3-1.ps1
+++ 3-1.ps1
@@ -1,8 +1,7 @@
 $index = @()
-$scores = 1..52
 $points_total = 0
 $alpha_int = 0
-foreach ($score in $scores) {
+for ($score = 1; $score -lt 53; $score++) {
     if ($score -lt 27) {
         $alpha_int = ($score + 96)
     } else {
@@ -12,7 +11,7 @@ foreach ($score in $scores) {
         'Score' = $score
         'Item'  = [char]$alpha_int
     }
-$index += $record
+    $index += $record
 }
 Get-Content -Path .\3-1.txt | ForEach-Object -Process {
     $global:dup_item = ""
@@ -22,14 +21,14 @@ Get-Content -Path .\3-1.txt | ForEach-Object -Process {
     $sack_2 = ($_).Substring(($n-$half),$half)
     $sack_1_items = $sack_1.ToCharArray()
     $sack_2_index = 0..($half-1)
-    foreach ($sack_1_item in $sack_1_items) {
+    :sack_search foreach ($sack_1_item in $sack_1_items) {
         foreach ($sack_2i in $sack_2_index) {
             if ($sack_1_item -ceq $sack_2[$sack_2i]) {
                 $global:dup_item = $sack_1_item
-                break
+                break sack_search
             }
         }
     }
-    $points_total += ($index | Where-Object {($_).Item -ceq $global:dup_item} | Select-Object Score).Score
+    $points_total += ($index | Where-Object {($_).Item -ceq $global:dup_item}).Score
 }
 Write-Host $points_total -ForegroundColor DarkGreen
